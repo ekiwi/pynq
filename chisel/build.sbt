eccc8a1f53ff823ebf05af83d2d23240ed91c24c
Kevin Laeufer <ekiwi@berkeley.edu>
2017-11-03 20:06:17 -0700

    [chisel] more familiar indent style

--- chisel/build.sbt
+++ chisel/build.sbt
@@ -5,18 +5,17 @@ version := "0.1"
 scalaVersion := "2.12.3"
 
 resolvers ++= Seq(
-  Resolver.sonatypeRepo("snapshots"),
-  Resolver.sonatypeRepo("releases")
+	Resolver.sonatypeRepo("snapshots"),
+	Resolver.sonatypeRepo("releases")
 )
 
 // Provide a managed dependency on X if -DXVersion="" is supplied on the command line.
-val defaultVersions = Map(
-  "chisel3" -> "3.0.0-RC1",
-  "chisel-iotesters" -> "1.1.0-RC1"
-  )
+val defaultVersions = Map("chisel3" -> "3.0.0-RC1", "chisel-iotesters" -> "1.1.0-RC1")
 
 libraryDependencies ++= (Seq("chisel3","chisel-iotesters").map {
-  dep: String => "edu.berkeley.cs" %% dep % sys.props.getOrElse(dep + "Version", defaultVersions(dep)) })
+	dep: String => "edu.berkeley.cs" %% dep % sys.props.getOrElse(
+		dep + "Version", defaultVersions(dep))
+})
 
 scalaSource in Compile := baseDirectory.value / "src"
 scalaSource in Test := baseDirectory.value / "test"
--- chisel/test/GCDUnitTest.scala
+++ chisel/test/GCDUnitTest.scala
@@ -6,71 +6,68 @@ import chisel3.iotesters
 import chisel3.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester}
 
 class GCDUnitTester(c: GCD) extends PeekPokeTester(c) {
-  /**
-    * compute the gcd and the number of steps it should take to do it
-    *
-    * @param a positive integer
-    * @param b positive integer
-    * @return the GCD of a and b
-    */
-  def computeGcd(a: Int, b: Int): (Int, Int) = {
-    var x = a
-    var y = b
-    var depth = 1
-    while(y > 0 ) {
-      if (x > y) {
-        x -= y
-      }
-      else {
-        y -= x
-      }
-      depth += 1
-    }
-    (x, depth)
-  }
+	/**
+	* compute the gcd and the number of steps it should take to do it
+	*
+	* @param a positive integer
+	* @param b positive integer
+	* @return the GCD of a and b
+	*/
+	def computeGcd(a: Int, b: Int): (Int, Int) = {
+		var x = a
+		var y = b
+		var depth = 1
+		while(y > 0 ) {
+			if (x > y) {
+				x -= y
+			} else {
+				y -= x
+			}
+			depth += 1
+		}
+		(x, depth)
+	}
 
-  private val gcd = c
+	private val gcd = c
 
-  for(i <- 1 to 40 by 3) {
-    for (j <- 1 to 40 by 7) {
-      poke(gcd.io.value1, i)
-      poke(gcd.io.value2, j)
-      poke(gcd.io.loadingValues, 1)
-      step(1)
-      poke(gcd.io.loadingValues, 0)
+	for(i <- 1 to 40 by 3) {
+	for (j <- 1 to 40 by 7) {
+		poke(gcd.io.value1, i)
+		poke(gcd.io.value2, j)
+		poke(gcd.io.loadingValues, 1)
+		step(1)
+		poke(gcd.io.loadingValues, 0)
 
-      val (expected_gcd, steps) = computeGcd(i, j)
+		val (expected_gcd, steps) = computeGcd(i, j)
 
-      step(steps - 1) // -1 is because we step(1) already to toggle the enable
-      expect(gcd.io.outputGCD, expected_gcd)
-      expect(gcd.io.outputValid, 1)
-    }
-  }
+		step(steps - 1) // -1 is because we step(1) already to toggle the enable
+		expect(gcd.io.outputGCD, expected_gcd)
+		expect(gcd.io.outputValid, 1)
+	}
+	}
 }
 
 /**
-  * This is a trivial example of how to run this Specification
-  * From within sbt use:
-  * {{{
-  * testOnly example.test.GCDTester
-  * }}}
-  * From a terminal shell use:
-  * {{{
-  * sbt 'testOnly example.test.GCDTester'
-  * }}}
-  */
+ * This is a trivial example of how to run this Specification
+ * From within sbt use:
+ * {{{
+ * testOnly example.test.GCDTester
+ * }}}
+ * From a terminal shell use:
+ * {{{
+ * sbt 'testOnly example.test.GCDTester'
+ * }}}
+ */
 class GCDTester extends ChiselFlatSpec {
-  private val backendNames = if(firrtl.FileUtils.isCommandAvailable("verilator")) {
-    Array("firrtl", "verilator")
-  }
-  else {
-    Array("firrtl")
-  }
-  for ( backendName <- backendNames ) {
-    "GCD" should s"calculate proper greatest common denominator (with $backendName)" in {
-      Driver(() => new GCD, backendName) {
-        c => new GCDUnitTester(c)
-      } should be (true)
-    }
-  }
+	private val backendNames =
+		if(firrtl.FileUtils.isCommandAvailable("verilator")) {
+			Array("firrtl", "verilator") } else { Array("firrtl") }
+
+	for ( backendName <- backendNames ) {
+		"GCD" should s"calculate proper greatest common denominator (with $backendName)" in {
+			Driver(() => new GCD, backendName) {
+				c => new GCDUnitTester(c)
+			} should be (true)
+		}
+	}
 }
