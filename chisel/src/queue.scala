b1be0c8a6a875a2a67c6f89b28897e65025b0450
Kevin Laeufer <ekiwi@berkeley.edu>
2017-11-03 20:59:51 -0700

    [chisel] queue: minimal compiling test skeleton

--- chisel/src/queue.scala
+++ chisel/src/queue.scala
@@ -3,10 +3,10 @@ package pynq
 import chisel3._
 import chisel3.util._
 
-class Queue[+T <: Data](val depth: Int, data_t: T) extends Module {
+class Queue[T <: Data](val depth: Int, data_t: T) extends Module {
 	val io = IO(new Bundle {
-		val in   = Input(data_t)
-		val out  = Output(data_t)
+		val in   = Input(data_t.chiselCloneType)
+		val out  = Output(data_t.chiselCloneType)
 		val push_back = Input(Bool())
 		val pop_front = Input(Bool())
 		val full = Output(Bool())
---
+++ chisel/test/queue.test.scala
@@ -0,0 +1,39 @@
+// See LICENSE for license details.
+
+package pynq
+
+import chisel3._
+import chisel3.iotesters
+import chisel3.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester}
+
+class QueueUnitTester[T <: Data](queue: Queue[T]) extends PeekPokeTester(queue) {
+	// for(i <- 1 to 40 by 3) {
+	// for (j <- 1 to 40 by 7) {
+	// 	poke(gcd.io.value1, i)
+	// 	poke(gcd.io.value2, j)
+	// 	poke(gcd.io.loadingValues, 1)
+	// 	step(1)
+	// 	poke(gcd.io.loadingValues, 0)
+
+	// 	val (expected_gcd, steps) = computeGcd(i, j)
+
+	// 	step(steps - 1) // -1 is because we step(1) already to toggle the enable
+	// 	expect(gcd.io.outputGCD, expected_gcd)
+	// 	expect(gcd.io.outputValid, 1)
+	// }
+	// }
+}
+
+class GCDTester extends ChiselFlatSpec {
+	private val backendNames =
+		if(firrtl.FileUtils.isCommandAvailable("verilator")) {
+			Array("firrtl", "verilator") } else { Array("firrtl") }
+
+	for ( backendName <- backendNames ) {
+		"Queue" should s"behave like a bounded queue (with $backendName)" in {
+			Driver(() => new Queue(4, UInt(8.W)), backendName) {
+				queue => new QueueUnitTester(queue)
+			} should be (true)
+		}
+	}
+}
