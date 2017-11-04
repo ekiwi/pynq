b107d3296f474d707e0d2fd7dd12774916c31202
Kevin Laeufer <ekiwi@berkeley.edu>
2017-11-03 22:02:30 -0700

    [queue] first try, does not pass tests

--- chisel/src/queue.scala
+++ chisel/src/queue.scala
@@ -14,8 +14,27 @@ class Queue(val depth: Int, val width: Int) extends Module {
 		val len = Output(UInt(log2Ceil(depth).W))
 	})
 
-	io.out := 0.U
-	io.full := false.B
-	io.empty := true.B
-	io.len := 0.U
+	io.full := io.len === depth.U
+	io.empty := io.len > 0.U
+
+	val do_push = io.push_back && !io.full
+	val do_pop  = io.pop_front && !io.empty
+
+	val len = RegInit(0.U(log2Ceil(depth).W))
+	io.len := len
+	len := MuxCase(len, Seq(
+		(do_push && !do_pop ) -> (len + 1.U),
+		(do_pop  && !do_push) -> (len - 1.U)))
+
+	val mem = Mem(depth, UInt(width.W))
+	val max_address = (depth - 1).U
+	def next_address(a: UInt): UInt = Mux(a === max_address, 0.U, a + 1.U)
+
+	val read_address = RegInit(0.U(log2Ceil(depth).W))
+	io.out := mem(read_address)
+	read_address := Mux(do_pop, next_address(read_address), read_address)
+
+	val write_address = RegInit(0.U(log2Ceil(depth).W))
+	when(do_push) { mem(write_address) := io.in }
+	write_address := Mux(do_push, next_address(write_address), write_address)
 }
