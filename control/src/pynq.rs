62046bf43dbe738181614bc288fe58d00a98e3da
Kevin Laeufer <ekiwi@berkeley.edu>
2017-11-11 22:02:23 -0800

    [constrol] (untested) code to manage DMA buffers

--- control/src/pynq.rs
+++ control/src/pynq.rs
@@ -55,6 +55,92 @@ fn write_bitstream_data(buf : &[u8]) {
 	file.write_all(buf).expect("Failed to write bitstream to FPGA");
 }
 
+// Xilinx has their own little kitchensink driver that allows access to various
+// DMAs and allocation of continous chunks of physical memory from userspace.
+// It can be found in `drivers/staging/apf/` in the Xilinx linux kernel repository.
+// We only need it in order to allocate CMA buffers to use with our DMA.
+// To use the DMA, we don't use the kernel but write to physical memory directly.
+
+// from `drivers/staging/apf/xlnk-ioctl.h`
+const XLNK_IOCALLOCBUF : libc::c_ulong = 0xc0045802;
+const XLNK_IOCFREEBUF  : libc::c_ulong = 0xc0045803;
+
+// from `drivers/staging/apf/xlnk.h`
+#[repr(C)]
+struct AllocBufIoctlArg { len: u32, id: i32, phyaddr : u32, cacheable : u8 }
+#[repr(C)]
+struct FreeBufIoctlArg { id: u32, buf : u32 }
+
+/// thin wrapper arond the /dev/xlnk pseudo file
+struct Xlnk { fd : libc::c_int }
+impl Xlnk {
+	fn open() -> Self {
+		let pseudo_file = CString::new("/dev/xlnk").unwrap();
+		let fd = unsafe { libc::open(pseudo_file.as_ptr(), libc::O_RDWR) };
+		assert!(fd > -1, "Failed to open /dev/xlnk.");
+		Xlnk { fd }
+	}
+	fn alloc_buf(&mut self, length : usize, is_cacheable : bool) -> (u32, u32) {
+		let mut args = AllocBufIoctlArg {
+			len: length as u32, id: -1, phyaddr: 0,
+			cacheable: if is_cacheable { 1 } else { 0 }
+		};
+		let ret = unsafe { libc::ioctl(self.fd, XLNK_IOCALLOCBUF, &mut args) };
+		assert!(ret >= 0, "xlnk.alloc_buf failed!");
+		(args.id as u32, args.phyaddr)
+	}
+	fn free_buf(&mut self, id : u32) {
+		let mut args = FreeBufIoctlArg { id : id, buf: 0 };
+		let ret = unsafe { libc::ioctl(self.fd, XLNK_IOCFREEBUF, &mut args) };
+		assert!(ret >= 0, "xlnk.free_buf failed!");
+	}
+	unsafe fn mmap_buffer(&mut self, id : u32, length : usize) -> *mut libc::c_void {
+		let offset = id << 24;
+		let mm = libc::mmap(std::ptr::null_mut(), length,
+		                    libc::PROT_READ | libc::PROT_WRITE,
+		                    libc::MAP_SHARED | libc::MAP_LOCKED,
+		                    self.fd, offset as libc::c_long);
+		assert!(mm != libc::MAP_FAILED, "Failed to mmap DMA buffer.");
+		mm
+	}
+}
+impl Drop for Xlnk {
+	fn drop(&mut self) {
+		assert!(unsafe { libc::close(self.fd) } == 0, "Failed to close /dev/xlnk.");
+	}
+}
+
+pub struct DmaBuffer { id : u32, physical_addr : u32, data: *mut u8, size: usize }
+
+impl DmaBuffer {
+	pub fn allocate(size : usize) -> Self {
+		let mut xlnk = Xlnk::open();
+		let (id, physical_addr) = xlnk.alloc_buf(size, false);
+		let data = unsafe { xlnk.mmap_buffer(id, size) } as *mut u8;
+		DmaBuffer { id, physical_addr, data, size }
+	}
+}
+impl Drop for DmaBuffer {
+	fn drop(&mut self) {
+		let mm = self.data as *mut libc::c_void;
+		assert!(unsafe { libc::munmap(mm, self.size) } == 0, "Failed to unmap DMA buffer.");
+		Xlnk::open().free_buf(self.id);
+	}
+}
+
+
+
+pub struct Dma {
+	mem : MemoryMappedIO
+}
+
+impl Dma {
+	pub fn get() -> Self {
+		let mem = MemoryMappedIO::map(0x40000000, 2 * 0x30);
+		Dma { mem }
+	}
+}
+
 #[derive(Copy, Clone, Debug)]
 pub enum Color {
 	Black = 0,
