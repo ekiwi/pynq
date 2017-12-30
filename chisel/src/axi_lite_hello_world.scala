021d247296a18e98e5afd3602e0fa9022b725a7f
Kevin Laeufer <ekiwi@berkeley.edu>
2017-12-30 14:31:26 +0100

    [axi] move follower interface to separate bundle

--- chisel/src/axi_lite_hello_world.scala
+++ chisel/src/axi_lite_hello_world.scala
@@ -3,36 +3,36 @@ package pynq
 import chisel3._
 import chisel3.util._
 
-class AxiLiteHelloWorld extends Module {
-	val width = 32
-	val bit_count = (width + 7) / 8
+class AxiLiteFollower extends Bundle {
+	val data_bits = 32
 	val address_bits = 4
 	val words = (1 << address_bits)
-	val io = IO(new Bundle {
-		// TODO: split up into separate bundles
-		// TODO: maybe use ReadyValid interface from stdlib
-		// read address interface
-		val s_axi_lite_araddr  = Input(UInt(address_bits.W))
-		val s_axi_lite_arready = Output(Bool())
-		val s_axi_lite_arvalid = Input(Bool())
-		// write address interface
-		val s_axi_lite_awaddr  = Input(UInt(address_bits.W))
-		val s_axi_lite_awready = Output(Bool())
-		val s_axi_lite_awvalid = Input(Bool())
-		// write response interface
-		val s_axi_lite_bresp  = Output(UInt(2.W))
-		val s_axi_lite_bready = Output(Bool())
-		val s_axi_lite_bvalid = Input(Bool())
-		// read data interface
-		val s_axi_lite_rdata = Output(UInt(width.W))
-		val s_axi_lite_rresp = Output(UInt(2.W))
-		val s_axi_lite_rready = Input(Bool())
-		val s_axi_lite_rvalid = Output(Bool())
-		// write data interface
-		val s_axi_lite_wdata = Input(UInt(width.W))
-		val s_axi_lite_wready = Output(Bool())
-		val s_axi_lite_wvalid = Input(Bool())
-	})
+	// TODO: maybe use ReadyValid interface from stdlib
+	// read address interface
+	val araddr  = Input(UInt(address_bits.W))
+	val arready = Output(Bool())
+	val arvalid = Input(Bool())
+	// write address interface
+	val awaddr  = Input(UInt(address_bits.W))
+	val awready = Output(Bool())
+	val awvalid = Input(Bool())
+	// write response interface
+	val bresp  = Output(UInt(2.W))
+	val bready = Output(Bool())
+	val bvalid = Input(Bool())
+	// read data interface
+	val rdata = Output(UInt(data_bits.W))
+	val rresp = Output(UInt(2.W))
+	val rready = Input(Bool())
+	val rvalid = Output(Bool())
+	// write data interface
+	val wdata = Input(UInt(data_bits.W))
+	val wready = Output(Bool())
+	val wvalid = Input(Bool())
+}
+
+class AxiLiteHelloWorld extends Module {
+	val io = IO(new AxiLiteFollower)
 }
 
 object AxiLiteHelloWorldGenerator extends App {
