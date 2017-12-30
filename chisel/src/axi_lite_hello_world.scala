9c474f897891d011f50e70e04217238718775bfd
Kevin Laeufer <ekiwi@berkeley.edu>
2017-12-30 20:24:35 +0100

    [axi] add untested address dependend reads of constants

--- chisel/src/axi_lite_hello_world.scala
+++ chisel/src/axi_lite_hello_world.scala
@@ -34,7 +34,7 @@ class AxiLiteFollower extends Bundle {
 // This is the simplest AxiLite Follower I could think of.
 // It does not accept writes and always returns a single constant, no matter what
 // address is read from.
-class AxiLiteReadOnceConstant extends Module {
+class AxiLiteReadOneConstant extends Module {
 	val io = IO(new AxiLiteFollower)
 
 	val magic = 0x12345678
@@ -55,6 +55,42 @@ class AxiLiteReadOnceConstant extends Module {
 
 }
 
+// This extends the above example to provide a different constant for
+// each address.
+class AxiLiteReadDifferentConstants extends Module {
+	val io = IO(new AxiLiteFollower)
+
+	val data = Seq(0x11111111, 0x2222222, 0x3333333, 0x4444444, 0x5555555,
+	               0x66666666, 0x7777777, 0x8888888, 0x9999999, 0xaaaaaaa)
+	val default = 0
+
+	val OK = 0.U
+
+	// read state
+	val sWaitForAddress :: sSendData :: Nil = Enum(2)
+	val read_state = RegInit(sWaitForAddress)
+	switch(read_state) {
+		is(sWaitForAddress) { when(io.arready && io.arvalid) { read_state := sSendData } }
+		is(sSendData) { when(io.rready && io.rvalid) { read_state := sWaitForAddress } }
+	}
+
+	// read
+	val read_address = RegInit(0.U(32.W))
+	when(io.arready && io.arvalid) { read_address := io.araddr }
+	io.arready := read_state === sWaitForAddress
+	io.rdata := MuxLookup(read_address, default.U, data.zipWithIndex.map{ case(d, ii) => ii.U -> d.U })
+	io.rresp := OK
+	io.rvalid := read_state === sSendData
+
+	// write
+	io.awready := false.B
+	io.bresp := 0.U // does not matter
+	io.bvalid := false.B
+	io.wready := false.B
+
+}
+
 object AxiLiteHelloWorldGenerator extends App {
-	chisel3.Driver.execute(args, () => new AxiLiteReadOnceConstant)
+	chisel3.Driver.execute(args, () => new AxiLiteReadOneConstant)
+	chisel3.Driver.execute(args, () => new AxiLiteReadDifferentConstants)
 }
